0d6836e33582e302b9f772a0d066559387baf416
Takao Mizuno <takao.mizuno110@gmail.com>
2021-05-09 21:50:38 +0900

    added basic profile info

--- src/components/contents/about/index/Profiles/index.tsx
+++ src/components/contents/about/index/Profiles/index.tsx
@@ -4,7 +4,45 @@ import React from 'react'
 const ObjectComponent: React.FC = () => {
   return (
     <div>
-      hoge
+      Takao Mizuno <br />
+      Jan 10th, 1994 <br />
+      Computer Lover / Book Lover <br />
+
+      lang
+      <ul>
+        <li>Python</li>
+        <li>TypeScript</li>
+      </ul>
+
+      tool
+      <ul>
+        <li>PyTorch</li>
+        <li>NumPy</li>
+        <li>Pandas</li>
+        <li>FastAPI</li>
+        <li>Django</li>
+        <li>Django REST Framework</li>
+        <li>React</li>
+        <li>Next.js</li>
+        <li>Redux</li>
+        <li>Recoil</li>
+        <li>Ant Design</li>
+        <li>Chakra-UI</li>
+        <li>Docker</li>
+        <li>Linux</li>
+        <li>AWS CDK</li>
+      </ul>
+
+      skill
+      <ul>
+        <li>Machine Learning</li>
+        <li>Audio Processing</li>
+        <li>Image Processing</li>
+        <li>Natural Language Processing</li>
+        <li>Frontend(SPA, SSR, SSG)</li>
+        <li>Backend(API)</li>
+        <li>Infra(AWS)</li>
+      </ul>
     </div>
   )
 }
--- src/components/contents/about/index/index.tsx
+++ src/components/contents/about/index/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import {
+  Box,
+  HStack
+} from '@chakra-ui/react'
 
 import Layout from 'components/layouts/main'
 import styles from 'styles/Home.module.css'
 import ProfileImage from './ProfileImage'
+import Profiles from './Profiles'
 
 
 const Content: React.FC = () => {
@@ -12,7 +17,14 @@ const Content: React.FC = () => {
         <main className={styles.main}>
           This page is gonna host a about page. <br />
           Plase coming soon..
-          <ProfileImage />
+          <HStack>
+            <Box>
+              <ProfileImage />
+            </Box>
+            <Box>
+              <Profiles />
+            </Box>
+          </HStack>
         </main>
       </div>
     </Layout>
