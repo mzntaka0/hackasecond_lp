85e1b34853d8ae88a3cff8f220e330e7f941f6c4
root <root@LAPTOP-AFME3DT9.localdomain>
2021-06-19 09:32:30 +0900

    updated nextjs

--- src/components/atoms/Logo/index.tsx
+++ src/components/atoms/Logo/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
 
-
 const Component: React.FC = () => {
   return (
     <div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import {NextPage} from 'next'
 
 import Content from 'components/contents/index'
 
-
 const Page: NextPage<{}> = () => {
   return <Content />
 }
