7f4ccce7288066db49c4b571c66fc857ad11ab5b
Takao Mizuno <takao.mizuno110@gmail.com>
2022-08-29 03:15:30 +0900

    deleted unnecessary codes

--- src/components/services/consultation/index.tsx
+++ src/components/services/consultation/index.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-import styles from "styles/Home.module.css";
-
 const Content: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
+    <div>
+      <main>
         This page is gonna host a service. <br />
         Plase coming soon..
       </main>
