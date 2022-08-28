3a6789228103ccd8ecba770fd464db0748b1dfe7
Takao Mizuno <takao.mizuno110@gmail.com>
2022-08-29 03:14:38 +0900

    deleted unnecessary codes

--- src/components/pricing/index/index.tsx
+++ src/components/pricing/index/index.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-import styles from "styles/Home.module.css";
-
 const Content: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
+    <div>
+      <main>
         This page is gonna host a pricing page. <br />
         Plase coming soon..
       </main>
